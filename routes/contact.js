0a2e379cf35c371b7e11a94872511065d0f1b9c0
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-03-12 07:16:03 -0400

    ADDED CONTACT DOCUMENTATION - in contact.js: - added swagger documentation to debug possible issue - ensured route and url is correct

--- routes/contact.js
+++ routes/contact.js
@@ -1,3 +1,31 @@
+/**
+ * @swagger
+ * /contact/submit:
+ *   post:
+ *     summary: Submit a contact form
+ *     tags: [Contact]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               subject:
+ *                 type: string
+ *               message:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Contact form submitted successfully
+ *       500:
+ *         description: Server error
+ */
+
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
